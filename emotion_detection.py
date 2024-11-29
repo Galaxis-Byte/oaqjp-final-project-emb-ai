cfb36a16daf2a9e4dbf450a648b7a7a5e31f8857
Galaxis-Byte <ashrox63@gmail.com>
2024-11-28 23:28:48 -0500

    Task 3 Update

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-def emotion_detection(text_to_analyse):
+def emotion_detector(text_to_analyse):
     # Information to access the Emotion Predict function of the Watson NLP Library
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -9,11 +9,13 @@ def emotion_detection(text_to_analyse):
 
     # Received response from the API
     response = requests.post(url, json=input_json, headers=headers)
-    
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        return json.dumps(data, indent=4)
-    else:
-        return ""
 
-print(emotion_detection("I love this new technology."))
+    # Convert response to JSON format
+    json_response = response.json()
+
+    # Construct the output
+    emotion_dict = json_response["emotionPredictions"][0]['emotion']
+    dominant_emotion = max(emotion_dict, key=emotion_dict.get)
+    emotion_dict["dominant_emotion"] = dominant_emotion
+
+    return emotion_dict
